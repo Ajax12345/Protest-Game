a917b3f88816641f11baedcc386f992d2ef8cc36
James Petullo <pentester18@gmail.com>
2019-07-08 15:35:36 -0400

    Update game_main.py

--- game_main.py
+++ game_main.py
@@ -1,13 +1,14 @@
 import flask, random, string
 import game_user, json, pusher
+import game_manager
 
 app = flask.Flask(__name__)
 app.secret_key = ''.join(random.choice(string.ascii_letters) for _ in range(30))
 
 pusher_client = pusher.Pusher(
-  app_id='....',
-  key='......',
-  secret='..........',
+  app_id='814342',
+  key='f7e3f6c14176cdde1625',
+  secret='5f4648c5a702b25bdb23',
   cluster='us2',
   ssl=True
 )
@@ -43,7 +44,29 @@ def login_user():
 @app.route('/game/<val>/<tuser>', methods=['GET'])
 @is_loggedin
 def game(val, tuser):
-    return flask.render_template('game_pannel.html', val = val, user = game_user.TestUser.get_user(flask.session['id']), tuser = tuser)
+    _result = game_manager.Game.load_game(int(val), tuser)
+    return '<h1>404</h1>' if isinstance(_result, dict) else flask.render_template('game_pannel1.html', game = _result)
+
+@app.route('/log_message')
+def log_message():
+    return flask.jsonify({'success':game_manager.Game.log_message(json.loads(flask.request.args.get('payload')))})
+
+@app.route('/get_message_history')
+def get_message_history():
+    return flask.jsonify({'html':flask.render_template('render_messages.html', messages = game_manager.Game.get_chat_history(json.loads(flask.request.args.get('payload'))))})
+
+@app.route('/get_gametime')
+def get_gametime():
+    return flask.jsonify({'time':game_manager.Game.get_gametime(json.loads(flask.request.args.get('payload')))})
+
+@app.route('/update_gametime')
+def update_gametime():
+    game_manager.Game.update_gametime(json.loads(flask.request.args.get('payload')))
+    return flask.jsonify({'success':'True'})
+
+@app.route('/get_scores')
+def get_scores():
+    return flask.jsonify({'scores':game_manager.Game.get_scores(json.loads(flask.request.args.get('payload')))})
 
 @app.route("/pusher/auth", methods=['POST'])
 def pusher_authentication():
