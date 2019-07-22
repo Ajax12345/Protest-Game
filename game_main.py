b7767c13ea9eba2da050f3f19c2e2dc809b2ff23
James Petullo <pentester18@gmail.com>
2019-07-21 22:03:31 -0400

    Update game_main.py

--- game_main.py
+++ game_main.py
@@ -1,9 +1,14 @@
 import flask, random, string
 import game_user, json, pusher
-import game_manager
+import game_manager, os, re
+from werkzeug import secure_filename
+
 
 app = flask.Flask(__name__)
 app.secret_key = ''.join(random.choice(string.ascii_letters) for _ in range(30))
+
+UPLOAD_FOLDER = 'class_rosters'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 pusher_client = pusher.Pusher(
   app_id='814342',
@@ -55,6 +60,10 @@ def log_message():
 def get_message_history():
     return flask.jsonify({'html':flask.render_template('render_messages.html', messages = game_manager.Game.get_chat_history(json.loads(flask.request.args.get('payload'))))})
 
+@app.route('/can_add_reaction')
+def can_add_reaction():
+    return flask.jsonify(game_manager.Game.can_add_reaction(json.loads(flask.request.args.get('payload'))))
+
 @app.route('/get_gametime')
 def get_gametime():
     return flask.jsonify({'time':game_manager.Game.get_gametime(json.loads(flask.request.args.get('payload')))})
@@ -88,6 +97,31 @@ def pusher_authentication():
         socket_id=flask.request.form['socket_id']
     )
     return json.dumps(auth)
+
+
+@app.route('/student_dashboard', methods=['GET'])
+def student_dashboard():
+    return flask.render_template('student_dashboard.html')
+
+@app.route('/create', methods=['GET'])
+def create():
+    return flask.render_template('new_game.html')
+
+@app.route('/add-class', methods=['GET'])
+def create_class():
+    return flask.render_template('create_class.html')
+
+@app.route('/test_post', methods=['POST'])
+def test_post():
+    _file = flask.request.files['myfile']
+    filename = secure_filename(_file.filename)
+    _id, _ext = len(os.listdir("class_rosters"))+1, re.findall("(?<=\.)\w+$", filename)[0]
+    _file.save(os.path.join(app.config['UPLOAD_FOLDER'], f'class_roster{_id}.{_ext}'))
+    return flask.redirect(f'/class/{game_manager.Classes.create_class(flask.request.form["classname"], _id)}')
+
+@app.route('/class/<id>', methods=['GET'])
+def get_class(id):
+    return f'<h1>class({id})</h1>'
 
 @app.after_request
 def add_header(r):
