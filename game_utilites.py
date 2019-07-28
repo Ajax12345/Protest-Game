978cb4bbe9727b202448791132d9d6ae3b9440f7
James Petullo <pentester18@gmail.com>
2019-07-28 12:15:01 -0400

    Update game_utilites.py

--- game_utilites.py
+++ game_utilites.py
@@ -1,4 +1,5 @@
-import typing, json
+import typing, json, functools
+import tigerSqlite
 
 def test_game_load(flag:bool = False) -> typing.Callable:
     def _inner(_f:typing.Callable) -> typing.Callable:
@@ -13,3 +14,23 @@ def validate_user(_f:typing.Callable) -> typing.Callable:
         _data = json.load(open('game_data.json'))
         return {'success':'False'} if not any(i['name'] == _user for i in _data['players']) else _f(cls, _data, _id, _user)
     return wrapper
+
+def validate_add_user(_f:typing.Callable) -> typing.Callable:
+    @functools.wraps(_f)
+    def wrapper(cls, _payload:dict) -> typing.Any:
+        _formatted = tigerSqlite.Sqlite('protest_users.db').get_id_data('users')
+        _option = [{'id':a, **b} for a, b in _formatted if all(b[j] == k for j, k in _payload.items())]
+        return _f(cls, _formatted, _payload) if not _option else cls(_option[0])
+    return wrapper
+
+
+if __name__ == '__main__':
+    d = json.load(open('game_data.json'))
+    d['time'] = '0:00'
+    d['board'] = []
+    d['rounds'] = []
+    d['round'] = []
+    d['score'] =  {"police": 0, "protesters": 0}
+    with open('game_data.json', 'w') as f:
+        json.dump(d, f)
+    
